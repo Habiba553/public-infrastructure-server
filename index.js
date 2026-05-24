d55b5d1f409dfa55f3e363a7ea74184406cd1833
Habiba553 <ummekabir684@gmail.com>
2026-05-24 23:10:43 +0600

    implemented admin backend apis

--- index.js
+++ index.js
@@ -170,6 +170,39 @@ async function run() {
     
       res.send(result);
     });
+    app.get('/admin/issues', async (req, res) => {
+
+      const result = await issuesCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .toArray();
+    
+      res.send(result);
+    });
+    app.patch('/issues/status/:id', async (req, res) => {
+
+      const id = req.params.id;
+    
+      const { status } = req.body;
+    
+      const filter = {
+        _id: new ObjectId(id)
+      };
+    
+      const updatedDoc = {
+    
+        $set: {
+          status
+        }
+      };
+    
+      const result = await issuesCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+    
+      res.send(result);
+    });
     // Get user by email
     app.get('/users/:email', async (req, res) => {
 
@@ -180,6 +213,71 @@ async function run() {
       const user = await usersCollection.findOne(query);
     
       res.send(user);
+    });
+
+    app.get('/users', async (req, res) => {
+
+      const result = await usersCollection
+        .find()
+        .toArray();
+    
+      res.send(result);
+    });
+    app.patch('/users/admin/:id', async (req, res) => {
+
+      const id = req.params.id;
+    
+      const filter = {
+        _id: new ObjectId(id)
+      };
+    
+      const updatedDoc = {
+    
+        $set: {
+          role: 'admin'
+        }
+      };
+    
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+    
+      res.send(result);
+    });
+    app.patch('/users/block/:id', async (req, res) => {
+
+      const id = req.params.id;
+    
+      const filter = {
+        _id: new ObjectId(id)
+      };
+    
+      const updatedDoc = {
+    
+        $set: {
+          blocked: true
+        }
+      };
+    
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+    
+      res.send(result);
+    });
+    app.delete('/users/:id', async (req, res) => {
+
+      const id = req.params.id;
+    
+      const query = {
+        _id: new ObjectId(id)
+      };
+    
+      const result = await usersCollection.deleteOne(query);
+    
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
